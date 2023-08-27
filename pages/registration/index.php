8f7819316be4b347174ccdc9329dfccf5962fa35
Alexander <alexanderbalabas1993@gmail.com>
2023-08-27 10:20:10 +0300

    add package.json

---
+++ pages/registration/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <form action="" class="col-10 col-sm-6 m-auto mt-5">
+        <input class="form-control form-control-sm mb-1" type="text" placeholder="login">
+        <input class="form-control form-control-sm mb-1" type="password" placeholder="password">
+        <input class="form-control form-control-sm mb-1" type="password" placeholder="confirm password">
+        <input class="form-control form-control-sm mb-1" type="email" placeholder="email">
+        <input class="form-control form-control-sm mb-1 btn btn-outline-success" type="submit">
+    </form>
+
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
